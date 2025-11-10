08321e248787a189d3d9a506a07ca2efb46115e6
Yash Kumar Halder <yashkumar667453@gmail.com>
2025-11-10 22:00:24 +0530

    feat(UI): Add related-showcase section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import BaseNavigation from "@/components/base-navigation";
 import Herohome from "@/components/hero-home";
+import RelatedShowcase from "@/components/related-showcase";
 
 export default function Home() {
   return (
     <div className="w-full h-screen ">
       <BaseNavigation />
       <Herohome />
+      <RelatedShowcase />
     </div>
   );
 }
---
+++ components/related-showcase.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const RelatedShowcase = () => {
+  return (
+    <div className="min-h-screen w-full bg-neutral-200 flex flex-col gap-18 px">
+      <div className="w-full h-[40vmin] bg-white" ></div>
+      <div className="w-full h-[40vmin] bg-white" ></div>
+      <div className="w-full h-[40vmin] bg-white" ></div>
+    </div>
+  );
+};
+
+export default RelatedShowcase;
